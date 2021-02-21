b256f69b13319750f4102230c9a6e0678084cfa6
TRNisbet <TRNisbet@hotmail.com>
2021-02-21 17:56:22 -0500

    final version

---
+++ index.js
@@ -0,0 +1,110 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+
+inquirer
+    .prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is your name?',
+        },
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of ths project?',
+        },
+        {
+            name: 'useCase',
+            type: 'input',
+            message: 'What is this application used for?',
+        },
+        {
+            name: 'install',
+            type: 'input',
+            message: 'What is needed to install the application?',
+        },
+        {
+            name: 'launch',
+            type: 'input',
+            message: 'How do you launch the application?',
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: 'What is your email?',
+        },
+        {
+            name: 'github',
+            type: 'input',
+            message: 'What is your gihub username?',
+        },
+         {
+            name: 'other',
+            type: 'input',
+            message: 'Is there any other information you want to add?',
+        },
+        {
+            name: 'license',
+            type: 'list',
+            message: 'How kind of license does this project have?',
+            choices: ["Apache 2.0", "GNU v3.0", "MIT"],
+        },
+    ])
+    .then((answer) => {
+        console.log('license', answer.license)
+        if (answer.license == 'Apache 2.0') {
+            var licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            var licenseDescription = 'Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0';
+        } else if (answer.license == 'GNU v3.0') {
+            var licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)';
+            var licenseDescription = 'This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.If not, see https://www.gnu.org/licenses/.';
+        } else if (answer.license == 'MIT') {
+            var licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            var licenseDescription = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.';
+        };
+        fs.writeFile('output/README.md',
+
+            `
+# ${answer.title}
+${licenseBadge}
+
+## Introduction
+
+This project was created by ${answer.name}. Their github can be found at https://github.com/${answer.github}.
+
+[Project Description](#project-description)  
+[Installation](#installation)
+[Running the App](#running-the-application)  
+[Questions](#questions)  
+[License](#license)  
+
+
+## Project Description  
+${answer.useCase}  
+
+
+## Installation  
+Before running the application make sure the following are already installed: 
+${answer.install}
+
+## Running the application 
+${answer.launch}
+
+## Other Information:
+${answer.other}
+
+
+## Questions
+The contact person for this application can be reached at ${answer.email}. Please reachout if you have comments, questions, or wish to contribute. 
+
+## License  
+About the ${answer.license} License:   
+${licenseDescription}
+`,
+
+            function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+    });
---
+++ output/README.md
@@ -0,0 +1,36 @@
+
+# TRN-Homework-09: ReadMe Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Introduction
+
+This project was created by Tom Nisbet. Their github can be found at https://github.com/TRNisbet.
+
+[Project Description](#project-description)  
+[Installation](#installation)
+[Running the App](#running-the-application)  
+[Questions](#questions)  
+[License](#license)  
+
+
+## Project Description  
+Easily create Readme files  
+
+
+## Installation  
+Before running the application make sure the following are already installed: 
+inquirer
+
+## Running the application 
+run node index.js from the terminal
+
+## Other Information:
+A video walkthrough is available here: https://drive.google.com/file/d/1vX9YoyCevSwJ3U4QLQxKN2tKbBJmHLEm/view and the GitHub Repo is available here: https://github.com/TRNisbet/TRN-Homework-09
+
+
+## Questions
+The contact person for this application can be reached at TRNisbet@hotmail.com. Please reachout if you have comments, questions, or wish to contribute. 
+
+## License  
+About the MIT License:   
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
